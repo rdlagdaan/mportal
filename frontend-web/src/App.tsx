84174f93a80254a25b65c3829668a4eb13b8b848
randylagdaan <rdlagdaan@gmail.com>
2025-08-12 19:14:25 +0800

    Detailed prototype files 22

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import DashboardLayout from './DashboardLayout'
 import TabbedLogin from './pages/TabbedLogin'      // <- make sure this path matches your file
 import CoursesPage from './pages/CoursesPage'
-import EnrolledPage from './pages/EnrolledPage.tsx.dev'
+import EnrolledPage from './pages/EnrolledPage'
 import FinishedPage from './pages/FinishedPage'
 import ProfilePage from './pages/ProfilePage'
 
@@ -19,7 +19,7 @@ export default function App() {
       <Route path="finished" element={<FinishedPage />} />
       <Route path="profile"  element={<ProfilePage />} />
     </Route>
-    <Route path="dashboard" element={<Navigate to="courses" replace />} />
+    <Route path="dashboard" element={<Navigate to="enrolled" replace />} />
     <Route path="*" element={<Navigate to="/login" replace />} /> {/* ✅ absolute */}
   </Routes>
 </BrowserRouter>
